46a581a6a6dee0320d131e1793d24a679864f48a
0001644bis <0001644bis@gmail.com>
2026-03-04 00:09:42 +0500

    Fix BASE_DIR and ALLOWED_HOSTS

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -12,10 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_URL = 'static/'
 ALLOWED_HOSTS = ['myangiboyev.pythonanywhere.com']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
