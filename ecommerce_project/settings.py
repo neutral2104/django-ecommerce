4ab02a25c48ca33324a27b3256d4265590368de8
0001644bis <0001644bis@gmail.com>
2026-03-04 01:59:40 +0500

    Fix BASE_DIR, STATIC_ROOT, and ALLOWED_HOSTS

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -13,12 +13,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 ALLOWED_HOSTS = ['myangiboyev.pythonanywhere.com']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
